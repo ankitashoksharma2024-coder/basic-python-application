5a291b86e1fb53cfc30f99ea48bdf07ffdb6246d
ankitashoksharma2024-coder <ankit.ashoksharma2024@vitstudent.ac.in>
2026-09-03 10:40:11 +0530

    first commit

--- app.py
+++ app.py
@@ -1,23 +1,40 @@
-import sys
-def add_numbers(a, b):
-    return a + b
-
-
-def run_addition():
-    if len(sys.argv) < 3:
-        print("Usage: python jenkins_pipeline_codes.py <num1> <num2>")
-        return
-
-    num1 = int(sys.argv[1])
-    num2 = int(sys.argv[2])
-    result = add_numbers(num1, num2)
-
-    print("=================================")
-    print("Addition Result")
-    print("=================================")
-    print(f"First Number : {num1}")
-    print(f"Second Number: {num2}")
-    print(f"Sum         : {result}")
-    
+def add(a, b):
+ return a + b
+def subtract(a, b):
+ return a - b
+def multiply(a, b):
+ return a * b
+def divide(a, b):
+ if b == 0:
+ raise ValueError("Cannot divide by zero")
+ return a / b
+def main():
+ while True:
+ print("\n--- MENU DRIVEN CALCULATOR ---")
+ print("1. Add")
+ print("2. Subtract")
+ print("3. Multiply")
+ print("4. Divide")
+ print("5. Exit")
+ choice = input("Enter your choice: ")
+ if choice == "5":
+ print("Exiting...")
+ break
+ if choice not in ["1", "2", "3", "4"]:
+ print("Invalid choice")
+ continue
+ a = float(input("Enter first number: "))
+ b = float(input("Enter second number: "))
+ if choice == "1":
+ print("Result:", add(a, b))
+ elif choice == "2":
+ print("Result:", subtract(a, b))
+ elif choice == "3":
+ print("Result:", multiply(a, b))
+ elif choice == "4":
+ try:
+ print("Result:", divide(a, b))
+ except ValueError as e:
+ print(e)
 if __name__ == "__main__":
-    run_addition()
+ main
